dcc95e4203c4309c9d9251a2cc11feabb89ffb07
roasmarialaura <roa20003@byui.edu>
2025-01-11 21:05:00 -0300

    Week01 exercise4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+        int totalSum = 0;
+        float average = 0;
+
+
+        while (userNumber!= 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber =int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        foreach(int number in numbers)
+        {
+            totalSum += number;
+        }
+
+        average =((float)totalSum) /numbers.Count;
+
+        int largest = numbers[0];
+        foreach( int number in numbers)
+        {
+            if(number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {totalSum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+        int smallest = largest;
+        foreach( int number in numbers)
+        {
+            if(number > 0 && number < smallest)
+            {
+                smallest = number;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+
+
+        Console.WriteLine("The sorted list is:");
+        numbers.Sort();
+        foreach(int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
